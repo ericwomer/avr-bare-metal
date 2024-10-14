6cd0ae9029c438f8d89c8a7dadf7c3a8f63404de
EWomer <ewomer@hotmail.com>
2024-10-14 07:41:40 -0400

    Working starting point for bare metal arduino (uno r3) programming

---
+++ README.md
@@ -0,0 +1,4 @@
+Bare metal avr programming (arduino uno r3) project 
+
+Code was taken from [Low Level](https://www.youtube.com/watch?v=5HgQkHzQc3o) on youtube
+Elements taken from [jpakkane/mesonarduino](https://github.com/jpakkane/mesonarduino/tree/master) as a template
--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,35 @@
 #include <avr/io.h>
-#include <util/delay.h>
+#include <avr/interrupt.h>
+
+// flip led every second
+
+// Clock Speed
+// #define F_CPU 16000000
+
+ISR (TIMER1_OVF_vect)
+{
+  PORTB ^= _BV(PORTB5);
+  TCNT1 = 65535 - (F_CPU/1024);
+}
 
 int main()
 {
-  DDRB = DDRB | (1 << DDB5);
+  DDRB |= _BV(DDB5);
+
+  // max value 65535
+  TCNT1 = 65535 - (F_CPU/1024);
+
+  // sets the prescaler to 1024
+  // 16M/1024 = 15625Hz
+  TCCR1B = (1 << CS10) | (1 << CS12);
+
+  TCCR1A = 0x00;
+
+  TIMSK1 = (1 << TOIE1);
+
+  sei();
+
   while(1) {
-    
-    PORTB = PORTB | ( 1 << PORTB5);
-    
-    _delay_ms(1000);
-
-    PORTB = PORTB & ~(1 << PORTB5);
-    _delay_ms(1000);
+    ;;
   }
-
-  return 0;
 }
