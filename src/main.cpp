583bd21884816e9620e053ec31fa7b91e7c9be35
EWomer <ewomer@hotmail.com>
2024-10-13 20:09:24 -0400

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main()
+{
+  DDRB = DDRB | (1 << DDB5);
+  while(1) {
+    
+    PORTB = PORTB | ( 1 << PORTB5);
+    
+    _delay_ms(1000);
+
+    PORTB = PORTB & ~(1 << PORTB5);
+    _delay_ms(1000);
+  }
+
+  return 0;
+}
